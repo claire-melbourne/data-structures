0ae51ea7d0d23763eb5fa15db9f08ff7b4ad395e
jeremyengland <jwengland2808@gmail.com>
2020-08-19 15:44:21 -0700

    Made size pass for empty object

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -13,7 +13,22 @@ var Queue = function() {
   };
 
   someInstance.size = function() {
+    // takes in an object, in this case storage
+    // if storage is empty, return 0
+    // set a variable (count) = 0
+    // loop over storage
+    // for each value, add 1 to count
+    // return counter
+    var count = 0;
+    if (storage === {}) {
+      return 0;
+    }
+    for (var key in storage) {
+      count += 1;
+    }
+    return count;
   };
 
   return someInstance;
 };
+
