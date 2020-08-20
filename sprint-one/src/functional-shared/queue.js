6df61a8db7d8b4b2a5a20f36d78ed53b5dccc8af
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-20 13:28:36 -0500

    plan functional shared class instatiation for queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,19 @@
 var Queue = function() {
+  var someQueue = {};
+  _.extend(someQueue, Queue.queueMethods);
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return someQueue;
 };
 
 var queueMethods = {};
+// Queue.queueMethods.enqueue = function () {}
+// Queue.queueMethods.dequeue = function () {}
+Queue.queueMethods.size = function () {
+  //set counter to 0
+  //loop over each key in someQueue
+  // counter ++;
+  return counter;
+};
 
 
