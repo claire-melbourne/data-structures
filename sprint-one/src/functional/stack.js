0ffeda33fe9e86ab6887c2cda3ce745282dbae4a
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-20 12:16:16 -0500

    implement size for functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -12,6 +12,20 @@ var Stack = function() {
   };
 
   someInstance.size = function() {
+    // takes in an object, in this case storage
+    // if storage is empty, return 0
+    // set a variable (count) = 0
+    // loop over storage
+    // for each value, add 1 to count
+    // return counter
+    var count = 0;
+    if (storage === {}) {
+      return 0;
+    }
+    for (var key in storage) {
+      count += 1;
+    }
+    return count;
   };
 
   return someInstance;
