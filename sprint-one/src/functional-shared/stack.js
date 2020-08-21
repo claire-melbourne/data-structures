5e2b4338dbee5aec302439682927bfe8158b601c
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-20 19:05:40 -0500

    implemented stack methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someStack = {};
+  someStack.storageSize = 0;
+  _.extend(someStack, stackMethods);
+  return someStack;
 };
 
 var stackMethods = {};
+stackMethods.pop = function () {
+  //delete current key
+  if (this.storageSize === 0) {
+    return 0;
+  }
+  var size = this.storageSize;
+  var key = this[size - 1];
+  delete this[size - 1];
+  this.storageSize --;
+  return key;
+};
+
+stackMethods.push = function (value) {
+//set var key to this.storageSize
+  var key = this.storageSize;
+  //set this[key] = value
+  this[key] = value;
+  this.storageSize ++;
+};
+
+stackMethods.size = function () {
+  return this.storageSize;
+};
 
 
