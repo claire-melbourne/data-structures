801fd22eb8ffb923a886e83d0fe841b9a2ee340d
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-20 19:34:55 -0500

    implement prototypal class queue constructor

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someQueue = Object.create(queueMethods);
+  someQueue.storageSize = 0;
+  return someQueue;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value) {
+  var key = this.storageSize;
+  this[key] = value;
+  this.storageSize += 1;
+};
+queueMethods.dequeue = function() {
+  var removed = this['0'];
+  if (this.storageSize === 0) {
+    return 0;
+  }
+  // use a for loop to iterate over the numeric keys in someQueue
+  for (var key = 0; key < this.storageSize; key++) {
+    var nextKey = key + 1;
+    if (!this[nextKey]) {
+      delete this[key];
+    } else {
+      this[key] = this[nextKey];
+    }
+  }
+  this.storageSize -= 1;
+  return removed;
+};
+queueMethods.size = function () {
+  return this.storageSize;
+};
 
 
