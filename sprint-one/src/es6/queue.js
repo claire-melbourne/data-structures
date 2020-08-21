f0af23e89b25597f857760199ed164388301bd76
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-20 21:03:46 -0500

    implement ES6 queue constructor

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,29 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storageSize = 0;
   }
-
+  enqueue(value) {
+    var key = this.storageSize;
+    this[key] = value;
+    this.storageSize ++;
+  }
+  dequeue() {
+    var removed = this['0'];
+    if (this.storageSize === 0) {
+      return 0;
+    }
+    for (var key = 0; key < this.storageSize; key ++ ) {
+      var nextKey = key + 1;
+      if (!this[nextKey]) {
+        delete this[key];
+      } else {
+        this[key] = this[nextKey];
+      }
+    }
+    this.storageSize --;
+    return removed;
+  }
+  size() {
+    return this.storageSize;
+  }
 }
