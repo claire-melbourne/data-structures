5d7021fb1e610de8b1acd51f8c2cd6010c35f500
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-20 20:15:36 -0500

    implement prototypal stack method

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someStack = Object.create(stackMethods);
+  someStack.storageSize = 0;
+  return someStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  this[this.storageSize] = value;
+  this.storageSize ++;
+};
+
+stackMethods.pop = function () {
+  if (this.storageSize === 0) {
+    return 0;
+  }
+  var removed = this[this.storageSize - 1];
+  delete this[this.storageSize - 1];
+  this.storageSize --;
+  return removed;
+};
+
+stackMethods.size = function () {
+  return this.storageSize;
+};
 
