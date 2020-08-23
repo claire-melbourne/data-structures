7cf9cc6627a5fd146ecc1a16ea4b57630cc7773b
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-22 22:11:13 -0500

    implemented hash table insert, retrieve and remove, collisions not resolved

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -10,38 +10,67 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // var bucket = this._storage[index]
-  var bucket = this._storage[index];
+  // console.log('val1 index: ', getIndexBelowMaxForKey('Steven', this._limit));
+  // console.log('val2 index: ', getIndexBelowMaxForKey('val2', this._limit));
   // if bucket is undefined
-  if (!bucket) {
+  if (!this._storage[index]) {
   // set value of this._storage.index to [];
-    bucket = [];
-    bucket.push([k, v]);
+    this._storage[index] = [];
+    this._storage[index].push([k, v]);
   } else {
-    for (var i = 0; i < bucket.length; i ++) {
-      if (bucket[i][0] === k) {
+    for (var i = 0; i < this._storage[index].length; i ++) {
+      if (this._storage[index][i][0] === k) {
       // tuple[1] = v
-        bucket[i][1] = v;
+        this._storage[index][i][1] = v;
       }
-      if (bucket[i][0] !== k && !bucket[i + 1][0]) {
-        bucket.push([k, v]);
+      if (this._storage[index][i][0] !== k && !this._storage[index][i + 1][0]) {
+        this._storage[index].push([k, v]);
       }
     }
   }
+  console.log('bucket: ', this._storage[index]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage[index]) {
+    // set value of this._storage.index to [];
+    this._storage[index] = [];
+  }
+  // console.log('bucket: ', bucket);
+  // iterate over bucket
+  for (var i = 0; i < this._storage[index].length; i++) {
+    // if bucket[i][0] === k
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //edge case for undefined bucket
+  //iterate over bucket
+  for (var i = 0; i < this._storage[index].length; i++) {
+    //if the element/tuple at index 0 equals k
+    if (this._storage[index][i][0] === k) {
+      var removedTuple = this._storage[index][i];
+      //slice? remove that element/tuple
+      console.log('before: ', this._storage[index]);
+      this._storage[index].splice(i, 1);
+      console.log('after: ', this._storage[index]);
+      //return removedTuple
+    }
+
+  }
+
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  Constant time even though there are occasional iterations through comparatively tiny bucket arrays
  */
 
 
